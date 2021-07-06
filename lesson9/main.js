efabd2e44522d99eb7ab50a11b0f171e3f336263
Fedorov Nikita <fedorovni1996@yandex.ru>
2021-07-06 23:15:37 +0500

    added tasks in lesson9

---
+++ lesson9/main.js
@@ -0,0 +1,23 @@
+// 1.  Дана строка. Сделайте заглавным первый символ этой строки не используя цикл. Найдите два решения
+
+// let str = "some very too long string";
+// console.log(str[0].toUpperCase() + str.slice(1));
+// console.log(str.split(' ')[0][0].toUpperCase() + str.slice(1));
+
+// 2. Дана строка, например, '123456'. Переверните эту строку (сделайте из нее '654321') не используя цикл
+
+// let str = '123456';
+// console.log(str.split('').reverse().join(''));
+
+// 3. Проверьте, что строка начинается на http://
+
+// let str = 'http://google.com';
+// console.log(str.indexOf('http://') === 0 ? true : false);
+// console.log(!str.indexOf('http://') && true);
+
+// 4. Проверьте, что строка заканчивается на .html
+
+// let str = 'index.html';
+
+// console.log(str.substr(str.length - 5, 5) === '.html' ? true : false);
+// console.log(str.substr(-5) === '.html' ? true : false);
