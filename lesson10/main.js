7706bd62cc3b1d593202c438c4ca2676feb381eb
Fedorov Nikita <fedorovni1996@yandex.ru>
2021-07-06 23:27:48 +0500

    added tasks in lesson10

---
+++ lesson10/main.js
@@ -0,0 +1,34 @@
+// 1. Сделайте функцию, которая возвращает квадрат числа. Число передается параметром.
+
+// function sqr(value) {
+//   return value ** 2;
+// }
+
+// console.log(sqr(5));
+// console.log(sqr(3));
+
+// 2. Сделайте функцию, которая возвращает сумму двух чисел.
+
+// function sum(firstNumber, secondNumber) {
+//   return firstNumber + secondNumber;
+// }
+
+// console.log(sum(3, 5));
+// console.log(sum(12, 7));
+
+// 3. Сделайте функцию, которая отнимает от первого числа второе и делит на третье.
+
+// function calc(firstNumber, secondNumber, thirdNumber) {
+//   return (firstNumber - secondNumber) / thirdNumber
+// }
+
+// console.log(calc(10, 2, 4));
+
+// 4. Сделайте функцию, которая принимает параметром число от 1 до 7, а возвращает день недели на русском языке.
+
+// function getDayOfWeek(day) {
+//   let dayOfWeek = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+//   return dayOfWeek[day - 1];
+// }
+
+// console.log(getDayOfWeek(6));
